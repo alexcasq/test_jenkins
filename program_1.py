e03fed93131d114fde68e6f534ef209a1334f991
Alexander Castillo <alexandercastilloq@gmail.com>
2017-04-10 15:40:09 +0200

    commit file program_1.py
    Commit: add ip scan to 172.18.13.132

--- program_1.py
+++ program_1.py
@@ -17,6 +17,22 @@ for host in nm.all_hosts():
         for port in lport:
             print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
 
+nm.scan('172.18.13.132', '22-443')
+
+for host in nm.all_hosts():
+    print('----------------------------------------------------')
+    print('Host : %s (%s)' % (host, nm[host].hostname()))
+    print('State : %s' % nm[host].state())
+    for proto in nm[host].all_protocols():
+        print('----------')
+        print('Protocol : %s' % proto)
+
+        lport = nm[host][proto].keys()
+        lport.sort()
+        for port in lport:
+            print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+
+
 print "finish app ---->"
 
 
