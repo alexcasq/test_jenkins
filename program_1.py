854e5370542f0d5ea00cce3d61049e5256800dd0
Alexander Castillo <alexandercastilloq@gmail.com>
2017-04-10 12:37:22 +0200

    add programm 1
    init commit

---
+++ program_1.py
@@ -0,0 +1,23 @@
+
+import nmap
+
+nm = nmap.PortScanner()
+nm.scan('172.18.13.175', '22-443')
+
+for host in nm.all_hosts():
+    print('----------------------------------------------------')
+    print('Host : %s (%s)' % (host, nm[host].hostname()))
+    print('State : %s' % nm[host].state())
+    for proto in nm[host].all_protocols():
+        print('----------')
+        print('Protocol : %s' % proto)
+
+        lport = nm[host][proto].keys()
+        lport.sort()
+        for port in lport:
+            print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+
+
+
+
+
