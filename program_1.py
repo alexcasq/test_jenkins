e1e08e6a8e808a455845683b7555728270534030
Alexander Castillo <alexandercastilloq@gmail.com>
2017-04-11 07:43:40 +0200

    Commit: program1
    Commit new ip

--- program_1.py
+++ program_1.py
@@ -32,6 +32,21 @@ for host in nm.all_hosts():
         for port in lport:
             print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
 
+nm.scan('172.18.13.130', '22-443')
+
+for host in nm.all_hosts():
+    print('----------------------------------------------------')
+    print('Host : %s (%s)' % (host, nm[host].hostname()))
+    print('State : %s' % nm[host].state())
+    for proto in nm[host].all_protocols():
+        print('----------')
+        print('Protocol : %s' % proto)
+
+        lport = nm[host][proto].keys()
+        lport.sort()
+        for port in lport:
+            print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+
 
 print "finish app ---->"
 
